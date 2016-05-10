872098fc406ec3abe22cd5b81959389050711721
sabazusi <kouhei.live@gmail.com>
2016-05-10 20:03:26 +0900

    add limit params for todos api.

--- app/api/v1/todos.rb
+++ app/api/v1/todos.rb
@@ -2,6 +2,9 @@ module V1
   class Todos < Grape::API
     resource ":todos" do
       desc "return todo items."
+      params do
+        optional :limit, type: Integer, default: 5
+      end
       get do
         Todo.all
       end
