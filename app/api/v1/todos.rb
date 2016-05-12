c741152f9c95390611998cc52901b87623e6433c
sabazusi <kouhei.live@gmail.com>
2016-05-13 00:46:43 +0900

    add offset, limit. fix typo.

--- app/api/v1/todos.rb
+++ app/api/v1/todos.rb
@@ -6,7 +6,7 @@ module V1
         optional :limit, type: Integer, default: 5
       end
       get do
-        Todo.all
+        Todo.order("date ASC").offset(0).limit(params[:limit])
       end
 
       desc "return todo items on specified month."
@@ -14,9 +14,12 @@ module V1
         requires :year, type: Integer
         requires :month, type: Integer
         optional :limit, type: Integer, default: 5
+        optional :offset, type: Integer, default: 0
       end
       get ':year/:month' do
-        Todo.my_month(params[:month], year: params[:year])
+        Todo
+          .by_month(params[:month], year: params[:year])
+          .order("date ASC").offset(params[:offset]).limit(params[:limit])
       end
     end
   end
